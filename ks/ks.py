98061c6e6cb12e75dc3ddc71f1777f75d984a62d
Colin Cotter <colin.cotter@imperial.ac.uk>
2024-08-02 15:42:01 +0100

    ks example

---
+++ ks/ks.py
@@ -0,0 +1,81 @@
+from firedrake import *
+
+ncells = 100
+L = 10.
+mesh = PeriodicIntervalMesh(ncells, L)
+
+V = FunctionSpace(mesh, "CG", 2)
+
+un = Function(V)
+unp1 = Function(V)
+
+uh = (un + unp1)/2
+
+v = TestFunction(V)
+
+dt = 0.01
+dT = Constant(dt)
+
+alpha = Constant(1.0)
+beta = Constant(0.02923)
+gamma = Constant(1.)
+
+h = avg(CellVolume(mesh))/FacetArea(mesh)
+n = FacetNormal(mesh)
+
+# C0 regularisation constant
+gamma = Constant(1.0)
+
+eqn = (
+    v*(unp1 - un)*dx
+    - dT*alpha*v.dx(0)*uh.dx(0)*dx
+    + dT*beta*(
+        v.dx(0).dx(0)*uh.dx(0).dx(0)*dx
+        + (dot(n("+"), dot(avg(grad(grad(v))), n("+"))))*jump(grad(uh), n)*dS
+        + (dot(n("+"), dot(avg(grad(grad(uh))), n("+"))))*jump(grad(v), n)*dS
+        + gamma/h*jump(grad(v), n)*jump(grad(uh), n)*dS
+               )
+    - dT*gamma*0.5*v.dx(0)*uh*uh*dx
+    )
+
+params = {
+    "snes_atol": 1.0e-50,
+    "snes_rtol": 1.0e-6,
+    "snes_stol": 1.0e-50,
+    "ksp_type":"preonly",
+    "pc_type":"lu"
+}
+
+#make the solver
+KSProb = NonlinearVariationalProblem(eqn, unp1)
+KSSolver = NonlinearVariationalSolver(KSProb,
+                                      solver_parameters=params)
+
+#initial condition
+
+x, = SpatialCoordinate(mesh)
+un.project(sin(pi*2*x))
+
+t = 0.
+tmax = 10.
+tdump = 1.
+dumpt = 0.
+
+file0 = File("stuff.pvd")
+VOut = FunctionSpace(mesh, "CG", 3)
+uout = Function(VOut)
+uout.interpolate(un)
+file0.write(uout)
+
+while t < tmax - dt/2:
+    t += dt
+
+    KSSolver.solve()
+    print(norm(unp1))
+    un.assign(unp1)
+
+    if dumpt > tdump - dt/2:
+        uout.interpolate(un)
+        file0.write(uout)
+        dumpt -= tdump
+    dumpt += dt
