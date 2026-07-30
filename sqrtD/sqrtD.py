1e6802d9c8512320738dcb28d82f10e3573da2b7
Colin Cotter <colin.cotter@imperial.ac.uk>
2026-07-30 17:41:25 +0100

    energy calcs and initial conditions.

--- sqrtD/sqrtD.py
+++ sqrtD/sqrtD.py
@@ -1,9 +1,11 @@
 from firedrake import *
 import irksome
 
-nx = 100
+nx = 10
 L = 100000
 mesh = PeriodicSquareMesh(nx, nx, L)
+mh = MeshHierarchy(mesh, 3)
+mesh = mh[-1]
 
 V = FunctionSpace(mesh, "BDM", 2)
 Q = FunctionSpace(mesh, "DG", 1)
@@ -12,11 +14,12 @@ Q = FunctionSpace(mesh, "DG", 1)
 W = V * V * V * Q
 
 # model parameters
+L = Constant(L)
 f = Constant(1e-4)
 g = Constant(10)
-H = Constant(1000)
-alpha = Constant(0)
-dt = 100.
+H = Constant(L/20)
+alpha = Constant(L/9)
+dt = Constant(0.5*L/nx/sqrt(g*H))
 
 n = FacetNormal(mesh)
 
@@ -27,7 +30,8 @@ U = Function(W)
 
 # initial conditions
 _, _, _, D = U.subfunctions
-D.interpolate(H-b)
+x, y = SpatialCoordinate(mesh)
+D.interpolate(H-b + 0.01*exp(-((x-L/2)**2 + (y-L/2)**2)/2/(L/9)**2))
 
 # equation system
 Dt = irksome.Dt
@@ -48,34 +52,27 @@ Upwind = 0.5 * (sign(dot(u, n)) + 1)
 
 # v_t equation
 eqn = inner(dv, Dt(v))*dx
-#eqn -= inner(perp(grad(inner(dv, perp(ubar)))), v)*dx
-#eqn += inner(both(perp(n)*inner(dv, perp(ubar))), both(Upwind*v))*dS
-#eqn += inner(dv, f*perp(ubar))*dx
+eqn -= inner(perp(grad(inner(dv, perp(ubar)))), v)*dx
+eqn += inner(both(perp(n)*inner(dv, perp(ubar))), both(Upwind*v))*dS
+eqn += inner(dv, f*perp(ubar))*dx
 eqn -= div(dv)*(
-    #inner(u, u)/2
-    + g*D
-    + g*b
-)*dx
+    inner(u, u)/2
+    + g*D + g*(D+b))*dx
 eqn -= div(dv)*(2/3)*alpha**2*div(F)*div(F)/D/D*dx
 # v-u relation
 eqn += (
-    inner(u
-          #-v
-          , du)*dx
+    inner(u-v, du)*dx
     + (2/3)*alpha**2*div(du)*div(F)/D*dx
     )
 # F definition
-eqn += inner(F-
-             H*u
-             #D*u
-             , dG)*dx
+eqn += inner(F-D*u, dG)*dx
 # D transport
 eqn += dD*(Dt(D)
            + div(F)
            )*dx
 
 # building a timestepper
-qd = 4
+qd = 10
 method = irksome.GalerkinCollocationScheme(
     order=1,
     stage_type="deriv",
@@ -88,17 +85,53 @@ t = MC.Constant(0.)
 
 scheme_J = irksome.GalerkinCollocationScheme(order=1)
 stepper = irksome.TimeStepper(eqn, method, t, dT, U,
-                              options_prefix="stepper")#,
-#scheme_J=scheme_J)
+                              options_prefix="stepper", scheme_J=scheme_J)
 
-nsteps = 50
+# coupled solver to construct u again for energy diagnostic
+VV = V * V
+du, dF = TestFunctions(VV)
+uF = Function(VV)
+u, F = split(uF)
+uFeqn = (
+    inner(u-v, du)*dx
+    + (2/3)*alpha**2*div(du)*div(F)/D*dx
+)
+uFeqn += (
+ inner(F-D*u, dF)*dx   
+)
+uFproblem = NonlinearVariationalProblem(uFeqn, uF)
+uFsolver = NonlinearVariationalSolver(uFproblem,
+                                      options_prefix="stepper")
+
+nsteps = 200
+tdump = 10
+dumpt = 0
 
 file = VTKFile('sqrtD.pvd')
 v, Iu, G, D = U.subfunctions
 eta = Function(Q).interpolate(D+b)
 file.write(v, eta)
 
+energy = []
+
 for step in ProgressBar('Timestep').iter(range(nsteps)):
+    F = stepper.solver._problem.F
+    with assemble(F).dat.vec_ro as vec:
+        res0 = vec.norm()
+        snes_rtol = stepper.solver.snes.rtol
+        stepper.solver.snes.ksp.atol = 0.1*snes_rtol*res0
+
     stepper.advance()
 
-    file.write(v, eta)
+    dumpt += 1
+    if dumpt == tdump:
+        dumpt = 0
+        v, _, _, D = split(U)
+        uFsolver.solve()
+        u, F = split(uF)
+        energy0 = assemble((0.5*D*inner(u,u)+g*D*(D/2+b)+
+                            alpha**2/3*inner(div(F),div(F)/D))*dx)
+        energy.append(energy0)
+        v, Iu, G, D = U.subfunctions
+        eta.interpolate(D+b)
+        file.write(v, eta)
